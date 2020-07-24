14c9a2930071bf2a99db5cade080d0574973aeb2
Frank Jia <jiafrank98@gmail.com>
2020-07-24 08:16:41 -0700

    stub out create success page

---
+++ src/views/components/SocialShareButton.vue
@@ -0,0 +1,72 @@
+<template>
+<!--TODO: Fill these in-->
+<ShareNetwork
+  class="social-share-button"
+  :network="network"
+  :url="url"
+  title="My Payment Page"
+  description=""
+  :style="{backgroundColor: color}"
+>
+  <FontAwesomeIcon :icon="icon" class="social-share-button-brand-icon"/>
+  <span class="social-share-button-label">{{ label }}</span>
+</ShareNetwork>
+</template>
+
+<script>
+// TODO: Need to finish
+import Vue from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VueSocialSharing from 'vue-social-sharing';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faFacebookF,
+);
+Vue.use(VueSocialSharing);
+
+export default {
+  name: 'SocialShareButton',
+  props: {
+    network: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: 'Share',
+    },
+  },
+  components: {
+    FontAwesomeIcon,
+  },
+  computed: {
+    icon() {
+      return faFacebookF;
+    },
+    color() {
+      return '#1877f2';
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.social-share-button {
+  cursor: pointer;
+  border: 1px solid gray;
+  padding: 0.2em;
+}
+.social-share-button-label {
+  color: white;
+}
+.social-share-button-brand-icon {
+  color: white;
+  margin-right: 0.5em;
+}
+</style>
--- src/views/create-success/CreateSuccessPage.vue
+++ src/views/create-success/CreateSuccessPage.vue
@@ -2,6 +2,28 @@
   <div class="create-success-page">
     <h1>Create Success!</h1>
     <p>{{ pageId }}</p>
+    <!--Badges-->
+    <div class="create-success-badges-section">
+      <h3>Share with Badges</h3>
+      <p>We use <a href="https://shields.io/">shields.io</a> to create shareable badges.</p>
+      <BRow no-gutters>
+        <!--TODO-->
+        <a href="#">
+          <img src="https://img.shields.io/badge/PayID-my%24pay.id-1877f2"/>
+        </a>
+      </BRow>
+    </div>
+    <!--Social Sharing-->
+    <div class="create-success-social-section">
+      <h3>Share on Social Media</h3>
+      <BRow no-gutters>
+        <SocialShareButton
+          network="facebook"
+          :url="pageFullUrl"
+          :label="'Share on Facebook'"
+        />
+      </BRow>
+    </div>
     <BaseButton
       label="Go to Your Page"
       :to="pagePath"
@@ -12,16 +34,20 @@
 <script>
 import { SUCCESS_PATH_QUERY_KEY, PAGE_PATH_BASE } from '../../constants/routes';
 import BaseButton from '../components/BaseButton.vue';
+import SocialShareButton from '../components/SocialShareButton.vue';
 
 export default {
   name: 'CreateSuccessPage',
-  components: { BaseButton },
+  components: { SocialShareButton, BaseButton },
   computed: {
     pageId() {
       return this.$route.query[SUCCESS_PATH_QUERY_KEY];
     },
     pagePath() {
       return `${PAGE_PATH_BASE}/${this.pageId}`;
+    },
+    pageFullUrl() {
+      return `${window.location.origin}${this.pagePath}`;
     },
   },
 };
